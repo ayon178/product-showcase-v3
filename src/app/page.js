9d14aa4f6e14120eb433fb8eec691e410bc870af
AyonJD <ayonjodder177@gmail.com>
2023-10-15 14:26:44 +0600

    All done

--- src/app/page.js
+++ src/app/page.js
@@ -14,68 +14,68 @@ import gsap from 'gsap'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
-   const [loadingStartTime, setLoadingStartTime] = useState(0)
+  const [loadingStartTime, setLoadingStartTime] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
 
-   const token =
-     typeof window !== 'undefined'
-       ? window.localStorage.getItem('product_v3_token')
-       : null
+  const token =
+    typeof window !== 'undefined'
+      ? window.localStorage.getItem('product_v3_token')
+      : null
 
-   useEffect(() => {
-     const navbar = document.querySelector('#navbar')
-     const text = document.querySelectorAll('.text_color')
-     const timeline = gsap.timeline({ paused: true })
+  useEffect(() => {
+    const navbar = document.querySelector('#navbar')
+    const text = document.querySelectorAll('.text_color')
+    const timeline = gsap.timeline({ paused: true })
 
-     timeline.to(navbar, {
-       // backgroundColor: 'red',
-       backdropFilter: 'blur(10px)',
-       duration: 0.1,
-       ease: 'power3.inOut',
-       backgroundColor: 'rgba(0, 0, 0, 0.5)',
-       marginTop: 0,
-       paddingBottom: '1rem',
-       paddingTop: '1rem',
-     })
+    timeline.to(navbar, {
+      // backgroundColor: 'red',
+      backdropFilter: 'blur(10px)',
+      duration: 0.1,
+      ease: 'power3.inOut',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      marginTop: 0,
+      paddingBottom: '1rem',
+      paddingTop: '1rem',
+    })
 
-     timeline.to(text, {
-       color: '#fff',
-       ease: 'power3.inOut',
-     })
+    timeline.to(text, {
+      color: '#fff',
+      ease: 'power3.inOut',
+    })
 
-     setLoadingStartTime(performance.now())
-     setTimeout(() => {
-       setIsLoading(false)
-     }, 1000)
+    setLoadingStartTime(performance.now())
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
 
-     const scrollListener = () => {
-       if (window.scrollY === 0) {
-         timeline.reverse()
-       } else {
-         timeline.play()
-       }
-     }
+    const scrollListener = () => {
+      if (window.scrollY === 0) {
+        timeline.reverse()
+      } else {
+        timeline.play()
+      }
+    }
 
-     window.addEventListener('scroll', scrollListener)
+    window.addEventListener('scroll', scrollListener)
 
-     return () => {
-       window.removeEventListener('scroll', scrollListener)
-     }
-   }, [])
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
 
-   useEffect(() => {
-     if (!isLoading) {
-       const loadingEndTime = performance.now()
-       const loadingTime = loadingEndTime - loadingStartTime
-       console.log(`Loading time: ${loadingTime} ms`)
-     }
-   }, [isLoading, loadingStartTime])
+  useEffect(() => {
+    if (!isLoading) {
+      const loadingEndTime = performance.now()
+      const loadingTime = loadingEndTime - loadingStartTime
+      console.log(`Loading time: ${loadingTime} ms`)
+    }
+  }, [isLoading, loadingStartTime])
 
-   if (!token) {
-     if (typeof window !== 'undefined') {
-       window.location.href = '/login'
-     }
-   }
-
+  if (!token) {
+    if (typeof window !== 'undefined') {
+      window.location.href = '/login'
+    }
+  }
 
   return (
     <>
